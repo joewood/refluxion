8cc030a376f04392e50387d57a7a47c900cfd6bc
Joe Wood <joewood@live.com>
2016-08-31 21:26:02 -0400

    Reset repo

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
